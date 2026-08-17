c2c66453020f1ffa527f70d48cdcb097958b2b5e
Akshat Bahuguna <bahugunaakshat618@gmail.com>
2026-08-17 15:15:34 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -85,18 +85,15 @@ mongoose
       "MongoDB connected"
     );
 
-    app.listen(
-      PORT,
-      () => {
-
-        console.log(
-          `Server running on port ${PORT}`
-        );
-
-      }
+   app.listen(
+  PORT,
+  "0.0.0.0",
+  () => {
+    console.log(
+      `Server running on port ${PORT}`
     );
-
-  })
+  }
+);
   .catch(error => {
 
     console.error(
