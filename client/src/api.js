5f60594d3c7a83b2d88d1752b07778dea797afd7
Akshat Bahuguna <bahugunaakshat618@gmail.com>
2026-08-17 14:07:58 +0530

    Update api.js

--- client/src/api.js
+++ client/src/api.js
@@ -1,26 +1,24 @@
 import axios from "axios";
 
-const API_URL = "https://job-board-p3jc.onrender.com";
+const API_URL = "https://job-board-p3jc.onrender.com/api";
+
+const API = axios.create({
+  baseURL: API_URL,
+});
 
 API.interceptors.request.use(
-  config => {
-
-    const token =
-      localStorage.getItem(
-        "job_token"
-      );
+  (config) => {
+    const token = localStorage.getItem("job_token");
 
     if (token) {
-
-      config.headers.Authorization =
-        `Bearer ${token}`;
-
+      config.headers.Authorization = `Bearer ${token}`;
     }
 
     return config;
-
+  },
+  (error) => {
+    return Promise.reject(error);
   }
 );
-
 
 export default API;
